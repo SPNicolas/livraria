28e69eb487576ac3a269088a7f48fc5406f035c3
SPNicolas <nicolassteffenspereira@gmail.com>
2025-04-11 15:13:47 -0300

    feat: criação da API para Editora

--- app/urls.py
+++ app/urls.py
@@ -7,14 +7,15 @@ from drf_spectacular.views import (
 )
 from rest_framework.routers import DefaultRouter
 
-from core.views import UserViewSet, CategoriaViewSet, EditoraViewSet, AutorViewSet
+from core.views import UserViewSet, CategoriaViewSet, EditoraViewSet, AutorViewSet, LivroViewSet
 
 router = DefaultRouter()
 
 router.register(r'usuarios', UserViewSet, basename='usuarios')
 router.register(r"categorias", CategoriaViewSet)
 router.register(r"editoras", EditoraViewSet)
-router.register(r"Autores", AutorViewSet)
+router.register(r"autores", AutorViewSet)
+router.register(r"livros", LivroViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- core/models/autor.py
+++ core/models/autor.py
@@ -1,8 +1,13 @@
+from tabnanny import verbose
 from django.db import models
 
 class Autor(models.Model):
     nome = models.CharField(max_length=100)
-    email = models.EmailField(max_length=100, null=True)
+    email = models.EmailField(max_length=100, null=True, blank=True)
 
     def __str__(self):
-        return f"{self.nome},    {self.email}"
+        return f"({self.id}){self.nome}, {self.email}"
+    
+    class Meta:
+        verbose_name = "Autor"
+        verbose_name_plural = "Autores"
