7dbb1fb32b6e8db249dfeb5893751fce66c3317e
SPNicolas <nicolassteffenspereira@gmail.com>
2025-07-15 08:27:11 -0300

    feat: criação de um endpoint para criar novas compras

--- core/views/compra.py
+++ core/views/compra.py
@@ -1,9 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 
+from core.serializers import CompraSerializer, CompraCreateUpdateSerializer
 from core.models import Compra
-from core.serializers import CompraSerializer
-
-
+...
 class CompraViewSet(ModelViewSet):
     queryset = Compra.objects.all()
     serializer_class = CompraSerializer
+
+    def get_serializer_class(self):
+        if self.action in ('create', 'update'):
+            return CompraCreateUpdateSerializer
+        return CompraSerializer
